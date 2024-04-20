936584ccc94df27c7d27d63b6883ce7f9c6edbaf
Anton <danilovanton724@gmail.com>
2024-04-20 16:02:35 +0200

    Initial Commit

---
+++ aviator_game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  aviator_game
+//
+//  Created by Anton on 20/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aviator_game/aviator_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  aviator_gameApp.swift
+//  aviator_game
+//
+//  Created by Anton on 20/4/24.
+//
+
+import SwiftUI
+
+@main
+struct aviator_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
